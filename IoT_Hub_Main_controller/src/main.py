909fdbb8b26e9fa32abac02ab053541f27f04f0e
SwapnilV <veerswapnil00@gmail.com>
2025-12-17 15:26:03 +0530

    main.py wiring

--- IoT_Hub_Main_controller/src/main.py
+++ IoT_Hub_Main_controller/src/main.py
@@ -2,23 +2,38 @@ import config
 config.cleanup_gpio()
 config.setup_gpio()
 
+from main_controller import MainController
+
 import threading
 from settings import SENSORS
 from file_manager import log_sensors
-from modules.phase_monitor import LedMonitor
+from modules.phase_monitor import LedMonitor, phase_data
 from modules.lcd_display.lcd_module import LCD
 import time
 from logging_config import logger
 from modules.pump_control import pump_manager
 from command_processor import processor_thread, processor
 from modules.sim800l.sim import sms_thread
-from modules.sim800l.sms_processor import sms_processor
+from modules.sim800l.sms_processor import sms_processor, set_controller
+
+# Create MainController (central brain)
+main_controller = MainController(
+    command_processor=processor,
+    phase_data=phase_data,
+    sms=sms_thread,
+)
+
+# Wire CommandProcessor -> MainController for events (आधी event_handler field add केलेला असेल तर)
+processor.event_handler = main_controller.handle_event
+
+# Wire SMS side -> MainController for incoming commands
+set_controller(main_controller)
+
 
 # sim800l = SIM800L()
 time.sleep(2)
 LCD()           # Lcd thread started
 time.sleep(2)
-sms_thread                       # sms thread started
 
 thread = threading.Thread(target=sms_processor, daemon=True)
 thread.start()  
