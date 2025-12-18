a3aeff6c9de2df9066b5d907578c1fd92e7064e9
SwapnilV <veerswapnil00@gmail.com>
2025-12-18 22:51:09 +0530

    sim.py cleaned up

--- IoT_Hub_Main_controller/src/modules/sim800l/sms_processor.py
+++ IoT_Hub_Main_controller/src/modules/sim800l/sms_processor.py
@@ -3,16 +3,20 @@ import threading
 import time
 from .sim import sms_thread, sms_queue
 main_controller = None
+_sms_lock = threading.Lock()  # shared lock
 
 def set_controller(controller):
     global main_controller
     main_controller = controller
 
 
-def msg_parser(queue = sms_queue):
-    while len(queue) > 0:
-        with threading.Lock():
+def msg_parser(queue=sms_queue):
+    while True:
+        with _sms_lock:
+            if not queue:
+                break
             sms = queue.pop(0)
+
         sender = sms["Number"]
         text = sms["Text"].strip()
 
@@ -25,9 +29,9 @@ def msg_parser(queue = sms_queue):
         except Exception as e:
             logger.exception("Error in handle_incoming_sms")
             reply = "Internal error. Please try again later."
+
         if reply:
             sms_thread.send_sms(number=sender, text=reply)
-
             
 def sms_processor():
     while True:
